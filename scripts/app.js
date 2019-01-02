9b657eb40b3af404a01116da3cc65fca67722944
Andre Stackhouse <captainstack@gmail.com>
2019-01-02 15:07:29 -0800

    update service worker location

--- scripts/app.js
+++ scripts/app.js
@@ -364,7 +364,7 @@
   // TODO add service worker code here
   if ('serviceWorker' in navigator) {
     navigator.serviceWorker
-             .register('https://captainstack.github.io/progressive-web-app-test/service-worker.js')
+             .register('./service-worker.js')
              .then(function() { console.log('Service Worker Registered'); });
   }
 })();
